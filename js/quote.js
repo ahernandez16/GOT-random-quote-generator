53ec447b1b94f7c520658fb6215882204cd14077
ahernandez16 <art3her@gmail.com>
2016-11-16 12:44:25 -0600

    Deleted glyphicons, added tweet button that autofills with quote, need to fix bugs next...

--- js/quote.js
+++ js/quote.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
 	$.getJSON("https://got-quotes.herokuapp.com/quotes.json", function(data){
 			var html = "";
+			var tweet = "";
 				html += "<blockquote class='got-quote'>";
 				html += "<p>" + data.quote + "</p>";
 				html += "<footer><cite title='got-char'>" + data.character + "</cite></footer>";
 				html += "</blockquote>";
+
+				tweet += "<a class='tweet-link btn btn-primary' href='https://twitter.com/intent/tweet?";
+				tweet += "text=" + encodeURIComponent(data.quote) + "+—" + encodeURIComponent(data.character) + "'>";
+				tweet += "<i class='fa fa-twitter fa-lg'></i> Tweet</a>";
 			$(".quote").html(html);
+			$(".tweet-button").html(tweet);
 		});
 	
 	$("#newQuote").on("click", function(){
 		$.getJSON("https://got-quotes.herokuapp.com/quotes.json", function(data){
 			var html = "";
+			var tweet = "";
 				html += "<blockquote class='got-quote'>";
 				html += "<p>" + data.quote + "</p>";
 				html += "<footer><cite title='got-char'>" + data.character + "</cite></footer>";
 				html += "</blockquote>";
+
+				tweet += "<a class='tweet-link btn btn-primary' href='https://twitter.com/intent/tweet?";
+				tweet += "text=" + encodeURIComponent(data.quote) + "+—" + encodeURIComponent(data.character) + "'>";
+				tweet += "<i class='fa fa-twitter fa-lg'></i> Tweet</a>";
 			$(".quote").html(html);
+			$(".tweet-button").html(tweet);
 		});
 	});
 }); 
