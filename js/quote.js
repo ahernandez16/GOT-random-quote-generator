bede2b0e1c7bd511a288aa0b2c7ce8f21d6732f0
ahernandez16 <art3her@gmail.com>
2016-11-17 21:19:02 -0600

    Fixed Twitter button bugs, also fixed double and single quotes issues.

--- js/quote.js
+++ js/quote.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 			var html = "";
 			var tweet = "";
 				html += "<blockquote class='got-quote'>";
-				html += "<p>" + data.quote + "</p>";
+				html += "<p>&ldquo;" + data.quote + "&rdquo;</p>";
 				html += "<footer><cite title='got-char'>" + data.character + "</cite></footer>";
 				html += "</blockquote>";
 
 				tweet += "<a class='tweet-link btn btn-primary' href='https://twitter.com/intent/tweet?";
-				tweet += "text=" + encodeURIComponent(data.quote) + "+—" + encodeURIComponent(data.character) + "'>";
+				tweet += "text=\"" + encodeURIComponent(data.quote).replace(/'/g, "%27") + "\"+—" + encodeURIComponent(data.character) + "'>";
 				tweet += "<i class='fa fa-twitter fa-lg'></i> Tweet</a>";
 			$(".quote").html(html);
 			$(".tweet-button").html(tweet);
@@ -19,12 +19,12 @@ $(document).ready(function() {
 			var html = "";
 			var tweet = "";
 				html += "<blockquote class='got-quote'>";
-				html += "<p>" + data.quote + "</p>";
+				html += "<p>&ldquo;" + data.quote + "&rdquo;</p>";
 				html += "<footer><cite title='got-char'>" + data.character + "</cite></footer>";
 				html += "</blockquote>";
 
 				tweet += "<a class='tweet-link btn btn-primary' href='https://twitter.com/intent/tweet?";
-				tweet += "text=" + encodeURIComponent(data.quote) + "+—" + encodeURIComponent(data.character) + "'>";
+				tweet += "text=\"" + encodeURIComponent(data.quote).replace(/'/g, "%27") + "\"+—" + encodeURIComponent(data.character) + "'>";
 				tweet += "<i class='fa fa-twitter fa-lg'></i> Tweet</a>";
 			$(".quote").html(html);
 			$(".tweet-button").html(tweet);
